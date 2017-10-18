7664e46bb7666ca8849879e3674b02591e794a27
Ethan Reed <32504090+dalaxia@users.noreply.github.com>
2017-10-18 18:11:12 -0500

    Update keySoftware.py

--- keySoftware.py
+++ keySoftware.py
@@ -1,5 +1,12 @@
 from generateKey import generateKey
+from keyMaker import keyMaker
+
 class keyInitiate():
-    key = generateKey.generateKeyWhole()
-    print(key)
-    
+    answer = input("What would you like to do? [G]enerate, [E]rase or [R]edeem keys.\n")
+    if(answer == "G"):
+        print(keyMaker.makeKeys())
+    elif(answer == "E"):
+        open("keys.txt", "w").close()
+        print("Keys erased!")
+    #elif(answer =="R"):
+        #redemption WIP
